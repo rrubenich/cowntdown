bc0903cb2fc7d1a1d100340623f31ac0541547ea
rrubenich <rfrubenich@gmail.com>
2025-08-19 18:29:11 -0300

    add some styles and PWA support

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = "countdown-talita-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/apple-touch-icon.png",
+];
+
+// Instalação do Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Cache aberto");
+        return cache.addAll(urlsToCache);
+      })
+      .catch((error) => {
+        console.log("Erro ao fazer cache dos arquivos:", error);
+      })
+  );
+});
+
+// Interceptação de requisições
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - retorna a resposta
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+// Atualização do Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log("Removendo cache antigo:", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
